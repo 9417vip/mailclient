808dd9abc59656c701b3be0ae5c2236822a763f9
runoneall <runoobsteve@gmail.com>
2026-06-29 13:17:17 +0800

    feat: enhance mail handling by adding account validation and configuration retrieval

--- src/mail.js
+++ src/mail.js
@@ -7,7 +7,31 @@ mail.use(needauth)
 import { getenv } from './cfenv.js'
 const env = getenv()
 
-import { WorkerMailer } from 'worker-mailer'
 import { CFImap } from 'cf-imap'
+import { WorkerMailer } from 'worker-mailer'
+
+mail.use(async (req, res, next) => {
+    const { account } = req.query
+    if (typeof account !== 'string' || account === '') {
+        return res.status(400).send('请求错误')
+    }
+
+    const result = await env.data.get(account)
+    if (!result) {
+        return res.status(404).send('账号不存在')
+    }
+
+    const config = JSON.parse(result)
+
+    req.emalAccount = {
+        address: account,
+        name: config.displayName
+    }
+
+    const authCredentials = {
+        username: account,
+        password: config.password
+    }
+})
 
 export default mail
